21f70985f9b42d34146e4dc5c0256bd6df01f568
remus432 <brosdev1@gmail.com>
2022-02-21 09:28:09 +0100

    initial project setup & config

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import AdviceCard from "./components/AdviceCard.svelte"
+</script>
+
+<main>
+  <AdviceCard />
+</main>
+
+<style lang="scss" global>
+  @use "./index" as *;
+</style>
---
+++ src/components/AdviceCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+
+<article class="advice-card">
+  <h1>Helloo</h1>
+</article>
+
+<style lang="scss">
+
+</style>
